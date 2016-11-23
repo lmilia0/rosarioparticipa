bdf3d250e9a3bfa8b00d4bd200f9652dd9c58938
Matías Lescano <mjlescano@protonmail.com>
2016-11-23 18:42:49 -0300

    use icon as arrow

--- ext/lib/site/topic-layout/topic-article/component.js
+++ ext/lib/site/topic-layout/topic-article/component.js
@@ -82,7 +82,9 @@ class TopicArticle extends Component {
         <div className='proyecto-main container'>
           <div className='row'>
             <div className='proyecto-content col-md-8'>
-              <Link className='volver' to='/presupuesto'>&lt; Ver todos los proyectos</Link>
+              <Link className='volver' to='/presupuesto'>
+                <i className='icon-arrow-left' />&nbsp;Ver todos los proyectos
+              </Link>
               {
                 topic.clauses && <Content clauses={topic.clauses} />
               }
