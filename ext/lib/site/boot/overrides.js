df5aefbade83fd346e40f6d01decc16211a85f3d
Francisco P <francisco@democracyos.io>
2016-11-24 15:58:38 -0300

    add comments lists overrides

--- ext/lib/site/boot/overrides.js
+++ ext/lib/site/boot/overrides.js
@@ -16,9 +16,18 @@ import * as TopicLayoutSidebarOverride from 'ext/lib/site/topic-layout/sidebar/c
 import * as TopicArticle from 'lib/site/topic-layout/topic-article/component'
 import * as TopicArticleOverride from 'ext/lib/site/topic-layout/topic-article/component'
 
+import * as CommentList from 'lib/site/topic-layout/topic-article/comments/list/component'
+import * as CommentListOverride from 'ext/lib/site/topic-layout/topic-article/comments-list/component'
+
+import * as CommentRepliesList from 'lib/site/topic-layout/topic-article/comments/list/replies/list/component'
+import * as CommentRepliesListOverride from 'ext/lib/site/topic-layout/topic-article/comments-replies-list/component'
+
+
 Object.assign(Layout, LayoutOverride)
 Object.assign(Header, HeaderOverride)
 Object.assign(HomeMultiforum, HomeMultiforumOverride)
 Object.assign(HomeForum, HomeForumOverride)
 Object.assign(TopicLayoutSidebar, TopicLayoutSidebarOverride)
 Object.assign(TopicArticle, TopicArticleOverride)
+Object.assign(CommentList, CommentListOverride)
+Object.assign(CommentRepliesList, CommentRepliesListOverride)
