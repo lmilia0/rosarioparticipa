70ed798b5e1654729c171cf7770db45f6d0625a4
Fran <francisco@democracyos.io>
2017-10-31 15:10:50 -0300

    fix voting banner visibility

--- ext/lib/site/banner-votacion/component.js
+++ ext/lib/site/banner-votacion/component.js
@@ -1,76 +1,49 @@
 import React, { Component } from 'react'
 import forumStore from 'lib/stores/forum-store/forum-store'
 
-
 export default class BannerVotacion extends Component {
-
   constructor (props) {
     super(props)
-    this.state={
-      visibility: true,
+    this.state = {
+      visibility: !sessionStorage.getItem('pp-17-banner'),
       stageVotacion: null,
       cierreVotacion: null
     }
   }
 
-  componentWillMount() {
+  componentWillMount () {
     forumStore.findOneByName('presupuesto')
     .then((forum) => {
       this.setState({
         stageVotacion: forum.extra.stage,
-        cierreVotacion: forum.extra.cierre
+        cierreVotacion: new Date(forum.extra.cierre)
       })
     })
-    .catch((err) => {
-      console.log(err)
-    })
-
-    localStorage.removeItem('bannerRendered')
-    this.checkFirstTime()
-    this.checkDate()
-  }
-
-  componentDidMount() {
-    this.state.visibility && localStorage.setItem('bannerRendered', true)
-  }
-
-  // se fija si es la primera vez que entraste
-  checkFirstTime = () => {
-    var alreadyRendered = localStorage.getItem('bannerRendered')
-    alreadyRendered && this.setState({visibility: false}, () => {console.log('visibility: ', this.state.visibility)})
-  }
-
-  // se fija la fecha
-  checkDate = () => {
-    var today = new Date()
-    var endDate = new Date(this.state.cierreVotacion)
-    if (Date.parse(endDate) - Date.parse(today) < 0) {
-      localStorage.removeItem('bannerRendered')
+    .catch(() => {
       this.setState({
         visibility: false
       })
-    }
+    })
   }
 
   closeBanner = (event) => {
-    this.setState({visibility: false})
+    sessionStorage.setItem('pp-17-banner', true)
+    this.setState({ visibility: false })
   }
 
-  render() {
-    let cierre = new Date(this.props.cierre).toLocaleDateString()
-    if (this.state.stageVotacion === 'votacion-abierta') {
-      return (
-        this.state.visibility && (
-          <div className='container-banner'>
-            <button className='closes' onClick={this.closeBanner}>x</button>
-            <h3>
-              ¡Ya está abierta la votación para el Presupuesto Participativo de este año!
-            </h3>
-            <h3>
-              Tenés tiempo hasta el {this.state.cierreVotacion.toString()}
-            </h3>
-          </div>
-        )
-      )} else {return null}
-    }
+  render () {
+    if (this.state.stageVotacion !== 'votacion-abierta') return null
+    const { cierreVotacion } = this.state
+    return this.state.visibility && (
+      <div className='container-banner'>
+        <button className='closes' onClick={this.closeBanner}>x</button>
+        <h3>
+          ¡Ya está abierta la votación para el Presupuesto Participativo de este año!
+        </h3>
+        <h3>
+          Tenés tiempo hasta el {cierreVotacion && `${cierreVotacion.getDate()}/${(cierreVotacion.getMonth() + 1)}`}
+        </h3>
+      </div>
+    )
   }
+}
