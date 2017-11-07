c357aa883a070f7564205afc6d575eaf86f1f755
Fran <francisco@democracyos.io>
2017-11-07 17:31:03 -0300

    fix card layout ie11

--- ext/lib/site/home-presupuesto/topic-grid/component.js
+++ ext/lib/site/home-presupuesto/topic-grid/component.js
@@ -74,7 +74,7 @@ export default class TopicGrid extends Component {
             )
           )}
         { // Grid Stage Votacion Abierta Presupuesto Participativo Adulto
-          (this.props.stage === 'votacion-abierta' && this.props.age === 'adulto') && (
+          (this.props.stage === 'votacion-abierta' && this.props.age.includes('adulto')) && (
             districts.map((district, i) =>
               // Div Distrito
               <div key={i}>
@@ -125,7 +125,7 @@ export default class TopicGrid extends Component {
             )
           )}
         { // Grid Stage Votacion Abierta Presupuesto Participativo Joven
-          (this.props.stage === 'votacion-abierta' && this.props.age === 'joven') && (
+          (this.props.stage === 'votacion-abierta' && this.props.age.includes('joven')) && (
             districts.map((district, i) =>
               <div key={i} className='topics-section'>
                 <Anchor id='distrito'>
