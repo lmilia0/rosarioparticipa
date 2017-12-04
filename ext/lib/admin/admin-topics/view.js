3a3440d5fea9ca12c63ae32c3ef0d5cb149d9ba3
franciclo <francisco@democracyos.io>
2017-12-03 22:45:53 -0300

    agrega paginacion

--- ext/lib/admin/admin-topics/view.js
+++ ext/lib/admin/admin-topics/view.js
@@ -8,9 +8,11 @@ import t from 't-component'
 import List from 'democracyos-list.js'
 import moment from 'moment'
 import confirm from 'democracyos-confirmation'
+import ReactPaginate from 'react-paginate'
 import urlBuilder from 'lib/url-builder'
 import View from 'lib/view/view'
 import topicStore from 'lib/stores/topic-store/topic-store'
+import { getQueryVariable } from 'lib/utils'
 import template from './template.jade'
 import UpdateStage from './update-stage/component'
 import ExportUpdate from 'lib/admin/admin-topics/export-update/component'
@@ -22,7 +24,7 @@ const log = debug('democracyos:admin-topics')
  */
 
 export default class TopicsListView extends View {
-  constructor (topics, forum = null) {
+  constructor (topics, forum = null, pagination) {
     super(template, {
       topics: topics.filter((t) => t.privileges.canEdit || t.privileges.canDelete),
       moment,
@@ -31,6 +33,7 @@ export default class TopicsListView extends View {
     })
 
     this.forum = forum
+    this.pagination = pagination
   }
 
   switchOn () {
@@ -46,6 +49,28 @@ export default class TopicsListView extends View {
         forum={this.forum} />), 
         this.el[0].querySelector('.export-update'))
     }
+    const pages = this.pagination.count / this.pagination.limit
+    const currentPage = (+getQueryVariable('page') || 1) - 1
+    ReactRender((
+      <ReactPaginate
+        forcePage={currentPage}
+        pageCount={pages}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        previousLabel='<'
+        nextLabel='>'
+        onPageChange={this.handlePageClick}
+        breakLabel={<a href="">...</a>}
+        breakClassName={"break-me"}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"} />
+    ), this.el[0].querySelector('.topics-pagination'))
+  }
+
+  handlePageClick (e) {
+    const { origin, pathname } = window.location
+    window.location = `${origin}${pathname}?page=${(e.selected + 1)}`
   }
 
   ondeletetopicclick (ev) {
